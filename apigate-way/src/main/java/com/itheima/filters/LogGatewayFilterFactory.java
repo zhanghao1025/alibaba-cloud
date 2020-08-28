177e97575c3cd0318867adcc8f7fce0b76f2e998
ZhangHao <761364201@qq.com>
2020-08-28 10:48:34 +0800

    test gateway filter

---
+++ apigate-way/src/main/java/com/itheima/filters/LogGatewayFilterFactory.java
@@ -0,0 +1,11 @@
+package com.itheima.filters;
+
+/**
+ * @author ZhangHao
+ * @Title: LogGatewayFilterFactory
+ * @Package com.itheima.filters
+ * @Description: 自定义过滤器
+ * @date 2020/8/2810:32
+ */
+public class LogGatewayFilterFactory {
+}
